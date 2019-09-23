9ad263b7b103a709d3d8a03f153119bca4fd82bc
javoter <javoter@mail.csuchico.edu>
2019-09-23 16:33:09 -0700

    here is your initials

--- main.cpp
+++ main.cpp
@@ -9,17 +9,20 @@ int main()
  string firstName;
  string lastName;
  
+
  cout <<" Enter first name ";
  cin >> firstName;
+ 
  cout <<" Enter last name ";
+
  cin >> lastName;
 
- cout <<"Welcome, "<<firstName<<" "<<lastName<<" here is your fortune...";
+ cout <<"Welcome, "<<firstName.at(0)<<" "<<lastName.at(0)<<" here is your fortune...";
 
 
-  //get user input
+ 
 
-  //tell fortune
+
 
   return 0;
 }
