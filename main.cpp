a52597b62d0260f957cdd9124e7f9b1ff749da0d
javoter <javoter@mail.csuchico.edu>
2019-09-25 17:18:34 -0700

    have a good day!

--- main.cpp
+++ main.cpp
@@ -1,29 +1,52 @@
 //Authors:
 #include<iostream>
 #include<string>
+#include<cctype>
 
 using namespace std;
 
 int main()
 {
- string firstName;
- string lastName;
- 
+        string firstName;
+        string lastName;
+        int index;
 
- cout <<"What is your first name?"<<endl;
- cin >> firstName;
- 
- cout <<"What is your last name?"<<endl;
+        cout <<"What is your first name?"<<endl;
+        cin >> firstName;
 
- cin >> lastName;
+        cout <<"What is your last name?"<<endl;
 
- cout <<"Welcome, "<<firstName.at(0)<<"."<<lastName.at(0)<<"., here is your fortune..."<<endl;
+        cin >> lastName;
 
- cout <<"your lucky number is "<<firstName.length()<<endl; 
+        cout <<"Welcome, "<<firstName.at(0)<<"."<<lastName.at(0)<<"., here is your fortune..."<<endl;
 
- 
+        cout <<"your lucky number is "<<firstName.length()<<endl;
 
+        index = lastName.length()-1;
 
+                if(firstName.at(0) == 'a' || firstName.at(0) == 'e' || firstName.at(0) == 'i' || firstName.at(0) == 'o' || firstName.at(0) == 'u' || firstName.at(0) == 'A' || firstName.at(0) == 'E' || firstName.at(0) == 'I' || firstName.at(0) == 'O' || firstName.at(0) == 'U')
+                { 
+                        cout <<"you are destined to be famous!"<<endl;
+                }
 
-  return 0;
+                else  
+                {
+                        cout <<"you should keep a low profile."<<endl;
+                }
+if(lastName.at(index) == 'A' || lastName.at(index) == 'a' || lastName.at(index) == 'E' || lastName.at(index) == 'e' || lastName.at(index) == 'I' || lastName.at(index) == 'i' || lastName.at(index) == 'O' || lastName.at(index) == 'o' || lastName.at(index) == 'U' || lastName.at(index) == 'u') 
+{
+        cout <<"you have already met your true love."<<endl;
 }
+
+
+    cout <<"have a good day!"<<endl;
+
+
+
+
+
+
+
+
+                return 0;
+        }
